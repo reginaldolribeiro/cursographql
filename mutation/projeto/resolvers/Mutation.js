b5c7c8e41b0cdb9430b5b010820d8c67f3bc7057
Reginaldo <reginaldolribeiro@gmail.com>
2019-06-20 20:42:53 -0300

    Update User

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -26,6 +26,27 @@ module.exports = {
         if(i < 0) return null
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos[0] : null
+    },
+
+    alterarUsuario(_, args){
+        const i = usuarios.findIndex(u => u.id === args.id)
+        if(i < 0) return null
+
+        // *** Alternativa 1
+        // const usuario = {
+        //     ...usuarios[i],
+        //     ...args
+        // }
+        // removendo elemento de indice "i: e adicionando novo elemento "usuario"
+        //usuarios.splice(i, 1, usuario)
+        //return usuario
+
+        // *** Alternativa 2
+        usuarios[i].nome = args.nome
+        usuarios[i].email = args.email
+        usuarios[i].idade = args.idade
+        return usuarios[i]
+
     }
 
     // novoUsuario(_, { nome, email, idade }){
