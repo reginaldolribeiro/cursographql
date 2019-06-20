41fec1ce0f2e673adbe80012587690530b83bd82
Reginaldo <reginaldolribeiro@gmail.com>
2019-06-20 20:03:16 -0300

    Email validate

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,12 +1,18 @@
 const { usuarios, proximoId } = require('../data/db')
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }){
+    novoUsuario(_, args){
+
+        const emailExistente = usuarios
+            .some(u => u.email === args.email)
+
+        if(emailExistente){
+            throw new Error('Email ja cadastrado')
+        }
+
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO'
         }
@@ -14,4 +20,17 @@ module.exports = {
         usuarios.push(novo)
         return novo
     }
+    // novoUsuario(_, { nome, email, idade }){
+    //     const novo = {
+    //         id: proximoId(),
+    //         nome,
+    //         email,
+    //         idade,
+    //         perfil_id: 1,
+    //         status: 'ATIVO'
+    //     }
+    //     console.log('Mutation ***')
+    //     usuarios.push(novo)
+    //     return novo
+    // }
 }
