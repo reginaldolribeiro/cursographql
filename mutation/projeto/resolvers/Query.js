ba20bc420b044c7275d487421dae82e2e3abdefc
reginaldo <reginaldolribeiro@gmail.com>
2019-06-21 15:02:37 -0300

    Input in Update User

--- mutation/projeto/resolvers/Query.js
+++ mutation/projeto/resolvers/Query.js
@@ -1,20 +1,31 @@
-const { usuarios, perfis } = require('../data/db')
+const { usuarios, perfis } = require("../data/db");
+
+function indiceUsuario(filtro) {
+  if (!filtro) return -1;
+  const { id, email } = filtro;
+  if (id) {
+    return usuarios.findIndex(u => u.id === id);
+  } else if (email) {
+    return usuarios.findIndex(u => u.email === email);
+  }
+  return -1;
+}
 
 module.exports = {
-    usuarios() {
-        return usuarios
-    },
-    usuario(_, { id }) {
-        const sels = usuarios
-            .filter(u => u.id === id)
-        return sels ? sels[0] : null
-    },
-    perfis() {
-        return perfis
-    },
-    perfil(_, { id }) {
-        const sels = perfis
-            .filter(p => p.id === id)
-        return sels ? sels[0] : null 
-    }
-}
+  usuarios() {
+    return usuarios;
+  },
+  usuario(_, { filtro }) {
+    const i = indiceUsuario(filtro);
+    if (i < 0) return null;
+    //return sels ? sels[0] : null;
+    return usuarios[i];
+  },
+  perfis() {
+    return perfis;
+  },
+  perfil(_, { id }) {
+    const sels = perfis.filter(p => p.id === id);
+    return sels ? sels[0] : null;
+  }
+};
