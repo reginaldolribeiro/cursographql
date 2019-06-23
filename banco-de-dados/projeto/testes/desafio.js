fa039cb1b27563135625658a256cc62a96528fb6
Reginaldo <reginaldolribeiro@gmail.com>
2019-06-23 19:13:32 -0300

    CRUD Usuario Perfil

--- banco-de-dados/projeto/testes/desafio.js
+++ banco-de-dados/projeto/testes/desafio.js
@@ -1,128 +1,89 @@
 const db = require('../config/db')
 
-const usuario = {
-    nome: "Ana",
-    email: "ana@empresa.com.br",
-    senha: "123"
-}
-
-// db('usuarios').insert(novoUsuario)
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err.sqlMessage))
-//     .finally(() => db.destroy())
-
-// db('usuarios').where({ email: "ana@empresa.com.br" })
-//     .first()
-//     .then(res => console.log(res))
-//     .finally(() => db.destroy())
-
-// const usuarioAlterado = db('usuarios').where({ email: "ana@empresa.com.br"})
-//             .update({
-//                 nome: "Ana alterada3",
-//                 email: "ana@empresa.com.br"
-//             })
-//             .then(res => console.log(res))
-//             .finally(() => db.destroy())
-
 async function salvarUsuario(nome, email, senha){
     
-    const usuario = await db('usuarios')
+    let usuario = await db('usuarios')
         .where({ email: email })
-        .first()
+        .first()    
     
+    // insert
+    if(!usuario){
+                
+        let [ id ] = await db('usuarios').insert({ nome, email, senha })
+        usuario = db('usuarios').where({ id }).first()
+
     // update
-    if(usuario){
-        
-        await db('usuarios').where({ email: email})
+    }else{        
+
+        await db('usuarios')
+            .where({ id: usuario.id})
             .update({
                 nome,
                 email,
                 senha
             })
-
-    // insert
-    }else{
-        
-        await db('usuarios').insert({
-            nome, email, senha
-        })
+        usuario = { ...usuario, nome, email, senha }
 
     }
       
-    return await db('usuarios').where({ email: email })
-            .first()
-            //.finally(() => db.destroy())
+    return usuario
 
 }
 
-//salvarUsuario("Reginaldo Luiz Ribeiro Filho", "reginaldo@empresa.com.br", "123456")
+// salvarUsuario("Joao de Souza", "joaosouza@empresa.com.br", "123456")
 //    .then(usuario => console.log(usuario))
 
 async function salvarPerfil(nome, rotulo){
 
-    const perfil = await db('perfis').where({ nome })
-        .first()
-        //.finally(() => db.destroy())
+    let perfil = await db('perfis').where({ nome }).first()
 
-    if(perfil){
+    if(!perfil){
         
-        await db('perfis').where({ nome })
-            .update({ nome, rotulo})
+        let [ id ] = await db('perfis').insert({ nome, rotulo })        
+        perfil = db('perfis').where({ id }).first()
 
     }else{
-        await db('perfis').insert({
-            nome,
-            rotulo
-        })
+        
+        await db('perfis').where({ id: perfil.id }).update({ nome, rotulo})
+        perfil = { ...perfil, nome, rotulo }
+
     }
 
-    return await db('perfis').where({ nome })
-        .first()
-        //.finally(() => db.destroy())
+    return perfil
 }
 
-// salvarPerfil('user','Usuario')
+// salvarPerfil('teste','Teste')
 //     .then(res => console.log(res))
 
 async function adicionarPerfis(usuario, ...perfis){
-    
-    for(perfil of perfis) {
-        console.log(perfil)
-        const { nome } = perfil
-        console.log(nome)
-        // const perfilBuscado = await db('perfis')
-        //                                 .select('id')
-        //                                 .where({ nome })
-        //                                 .first()
-        //                                 .then(res => console.log(res))
-        // console.log(perfilBuscado)
+
+    const usuario_id = usuario.id
+    await db('usuarios_perfis').where({ usuario_id }).delete()
+
+    for(perfil of perfis) {       
+        const perfil_id = perfil.id
+        await db('usuarios_perfis').insert({ usuario_id, perfil_id })
     }
 
 }
 
-adicionarPerfis(usuario, [{ nome: "admin", rotulo: "Administrador"},{ nome: "xxxxx", rotulo: "Administrador"}])
-    .then(res => console.log(res))
-    .finally(() => db.destroy())
-
-// db('perfis')
-//     .select('id')
-//     .where({ nome: "admin" })
-//     .first()
+// adicionarPerfis({ id: 2, nome: "Ana", email: "ana@empresa.com.br", senha: "123"}, 
+//                  [{ id: 16, nome: "admin", rotulo: "Administrador"},{ id: 18, nome: "user", rotulo: "Usuario"}])
 //     .then(res => console.log(res))
 //     .finally(() => db.destroy())
 
-// async function executar(){
-//     const usuario = await salvarUsuario("Ana", "ana@empresa.com.br", "123")
-//     const perfilA = await salvarPerfil("rh", "Pessoal")
-//     //const perfilB = await salvarPerfil("fin", "Financeiro")
+async function executar(){
+    const usuario = await salvarUsuario("Reginaldo Luiz Ribeiro Filho", "reginaldo@empresa.com.br", "123")
+    const perfilA = await salvarPerfil("rh1", "Pessoal")
+    const perfilB = await salvarPerfil("fin1", "Financeiro")
 
-//     console.log(usuario)
-//     console.log(perfilA)
-//     //console.log(perfilB)
+    console.log(usuario)
+    console.log(perfilA)
+    console.log(perfilB)
 
-//     //await adicionarPerfis(usuario, perfilA, perfilB)
-// }
+    await adicionarPerfis(usuario, perfilA, perfilB)
+}
 
-// executar()
-//     .catch(err => console.log(err))
-//     .finally(() => db.destroy())
+executar()
+    .catch(err => console.log(err))
+    .finally(() => db.destroy())
